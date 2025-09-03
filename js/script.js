f864977295e2cc168d88c89633c2805365c34455
Coolafdood <chazeldred55@gmail.com>
2025-09-03 10:38:24 +0100

    added the current list of pokemon and their images linked to the directory

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ const resultText = document.getElementById('resultText');
 const guessInput = document.getElementById('guessInput');
 const checkGuessButton = document.getElementById('checkGuessButton');
 const nextPokemonButton = document.getElementById('nextPokemonButton');
+
+const pokemonList = [
+  { name: "pikachu", img: "assets/images/pikachu.png" },
+  { name: "charmander", img: "assets/images/charmander.png" },
+  { name: "bulbasaur", img: "assets/images/bulbasaur.png" },
+  { name: "squirtle", img: "assets/images/squirtle.png" }
+];
