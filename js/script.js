f742d1866a30357dd76c93c30d91240f7a0a0704
Coolafdood <chazeldred55@gmail.com>
2025-09-03 13:32:43 +0100

    linked the js script to the html through the ids

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let pokemonImage = document.getElementById('pokemonImage');
+
+let guessInput = document.getElementById('guessInput');
+let resultText = document.getElementById('resultText');
+let checkGuessButton = document.getElementById('checkGuessButton');
+let nextPokemonButton = document.getElementById('nextPokemonButton');
+function runGame() {
+
+}
+
+function checkAnswer() {
+}
