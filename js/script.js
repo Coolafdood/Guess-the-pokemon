7349a7006a1935786cdb52f56d930b464189b077
Coolafdood <chazeldred55@gmail.com>
2025-09-03 13:33:55 +0100

    linked the script js to the src of the images

--- js/script.js
+++ js/script.js
@@ -10,3 +10,11 @@ function runGame() {
 
 function checkAnswer() {
 }
+let currentPokemonId = 0;
+
+const pokemonList = [
+  { name: "pikachu", img: "assets/images/pikachu.png" },
+  { name: "charmander", img: "assets/images/charmander.png" },
+  { name: "bulbasaur", img: "assets/images/bulbasaur.png" },
+  { name: "squirtle", img: "assets/images/squirtle.png" }
+];
